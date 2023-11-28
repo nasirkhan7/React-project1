36f576f2d1678f4f8fe978c0cf9147864861dc55
nasirkhan <nasirkhana842@gmail.com>
2023-11-28 04:13:58 -0500

    react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// old approach to make component
+//component always should return something
+
+// function Welcome(){
+//   return (
+//     <div>
+//       <h1>
+//       Hello World
+//       </h1>
+//       <p>Hello WorldHello World</p>
+//     </div>
+//   )
+// }
+
+const BooKList=()=>{
+  return(
+    <div>
+    <Book/>
+    </div>
+  )
+}
+const Book =()=>{
+  return (
+    <div>
+    <Image/>
+    <Title/>
+    </div>
+  )
+}
+
+const Image = ()=>{
+  return(
+    <div>
+      <img src='https://images-na.ssl-images-amazon.com/images/I/81WklxcuSZL._AC_UL254_SR254,254_.jpg' />
+    </div>
+  )
+}
+
+const Title = ()=>{
+  return (
+    <div>
+      <h3>This is the Title</h3>
+    </div>
+  )
+}
+
+const Price =()=>{
+  return(
+    <di>
+      <p></p>
+    </di>
+  )
+}
+
+
+
+
+
+ReactDOM.render(<BooKList/>,document.querySelector('#root'))
